85051a914c6034b7db84a84c445bd01e29db8059
Ivan Chapkailo <chapkailo.ivan@gmail.com>
2017-04-28 14:11:40 +0300

    add computer as player

---
+++ Blackjack/Blackjack/House.cpp
@@ -0,0 +1,26 @@
+#include "House.h"
+
+using namespace std;
+
+House::House(const string& name) : GenericPlayer(name)
+{
+}
+
+House::~House()
+{
+}
+
+bool House::IsHitting() const
+{
+	return (GetTotal() <= 16); // if there is less of 16 scores hit the card
+}
+
+void House::FlipFirstCard()
+{
+	// check excistense of card
+	//
+	if (!(m_Cards.empty()))
+		m_Cards[0]->Flip();
+	else
+		cout << "> FlipFirstCard(): No card to flip." << endl;
+}
---
+++ Blackjack/Blackjack/House.h
@@ -0,0 +1,18 @@
+// House.h >> declatration of class House inherited from class GenericPlayer (GenericPlayer.h/.cpp)
+// class House represents diller (casino, computer-player).
+#ifndef HOUSE_H
+#define HOUSE_H
+#include "stdafx.h"
+#include "GenericPlayer.h"
+
+class House : public GenericPlayer
+{
+public:
+	House(const std::string& name = "House");
+	virtual ~House();
+	virtual bool IsHitting() const;  // show whether palyer want to continue take cards
+	void FlipFirstCard(); // overturning first card
+};
+
+
+#endif // !HOUSE_H
