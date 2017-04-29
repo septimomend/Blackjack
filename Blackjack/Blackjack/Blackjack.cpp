e19fce2a02b2023eb818a4af767aee1a23239028
Ivan Chapkailo <chapkailo.ivan@gmail.com>
2017-04-29 14:26:44 +0300

    implement main cycle of game

--- Blackjack/Blackjack/Blackjack.cpp
+++ Blackjack/Blackjack/Blackjack.cpp
@@ -2,10 +2,42 @@
 //
 
 #include "stdafx.h"
+#include "Game.h"
 
+using namespace std;
+
+ostream& operator<<(ostream& os, const Card& aCard);
+ostream& operator<<(ostream& os, const GenericPlayer& aGenericPlayer);
 
 int main()
 {
+	cout << "\t\tBlackjack Console Game\n" << endl;
+	int numPlayers = 0;
+	while (numPlayers < 1 || numPlayers > 7)
+	{
+		cout << "How many players? (1-7) - ";
+		cin >> numPlayers;
+	}
+	vector<string> names;
+	string name;
+	for (int i = 0; i < numPlayers; ++i)
+	{
+		cout << "Enter player number " << i << " name: ";
+		cin >> name;
+		names.push_back(name);
+	}
+	cout << endl;
+	// cycle of game
+	Game aGame(names);
+	char again = 'y';
+	while (again != 'n' && again != 'N')
+	{
+		system("cls");
+		aGame.Play();
+		cout << "/nDo you want to play again& (Y/N): ";
+		cin >> again;
+	}
+	cout << "Good luck!" << endl;
     return 0;
 }
 
