0f7e580d3bebeb86c4bb5fdb5c092c174ba8764d
Ivan Chapkailo <chapkailo.ivan@gmail.com>
2017-04-29 14:13:09 +0300

    implement game algorithm using class Game

---
+++ Blackjack/Blackjack/Game.cpp
@@ -0,0 +1,78 @@
+// Game.cpp >> definition of class Game placed in Game.h file
+#include "stdafx.h"
+#include "Game.h"
+
+using namespace std;
+
+Game::Game(const vector<string>& names)
+{
+	vector<string>::const_iterator pName;
+	// create vector of players from vector of names. Create Player object for each name
+	//
+	for (pName = names.begin(); pName != names.end(); ++pName)
+		m_Player.push_back(Player(*pName));
+	// random generator
+	//
+	srand(static_cast<unsigned int>(time(0)));
+	m_Deck.Generate();
+	m_Deck.Shuffle();
+}
+
+Game::~Game()
+{
+}
+
+void Game::Play()
+{
+	vector<Player>::iterator pPlayer;
+	// deal two cards for everybody
+	//
+	for (int i = 0; i < 2; ++i)
+	{
+		for (pPlayer = m_Player.begin; pPlayer != m_Player.end(); ++pPlayer)
+			m_Deck.Deal(*pPlayer);
+		m_Deck.Deal(m_House);
+	}
+	m_House.FlipFirstCard(); // hide first diller's card
+	// open all player's hands
+	//
+	for (pPlayer = m_Player.begin; pPlayer != m_Player.end(); ++pPlayer)
+		cout << *pPlayer << endl;
+	cout << m_House << endl;
+	// deal additional cards for players
+	//
+	for (pPlayer = m_Player.begin; pPlayer != m_Player.end(); ++pPlayer)
+		m_Deck.AdditionalCards(*pPlayer);
+	m_House.FlipFirstCard(); // show diller's first card
+	cout << endl << m_House;
+	m_Deck.AdditionalCards(m_House); // deal additional cards for diller
+	if (m_House.IsBusted())
+	{
+		// all players remained at game are winners
+		//
+		for (pPlayer = m_Player.begin; pPlayer != m_Player.end(); ++pPlayer)
+		{
+			if (!(pPlayer->IsBusted()))
+				pPlayer->Win();
+		}
+	}
+	else
+	{
+		// compare sum of remaining player's scores and sum of diller's scores
+		//
+		if (!(pPlayer->IsBusted()))
+		{
+			if (pPlayer->GetTotal() > m_House.GetTotal())
+				pPlayer->Win();
+			else if (pPlayer->GetTotal() < m_House.GetTotal())
+				pPlayer->Lose();
+			else
+				pPlayer->Push();
+		}
+	}
+	// clear all player's hands
+	//
+	for (pPlayer = m_Player.begin; pPlayer != m_Player.end(); ++pPlayer)
+		pPlayer->Clear();
+	m_House.Clear();
+}
---
+++ Blackjack/Blackjack/Game.h
@@ -0,0 +1,22 @@
+// Game.h >> declaration of class Game
+// represent game Blackjack
+#ifndef GAME_H
+#define GAME_H
+#include "stdafx.h"
+#include "Deck.h"
+#include "House.h"
+#include "Player.h"
+
+class Game
+{
+public:
+	Game(const std::vector<std::string>& names);
+	~Game();
+	void Play(); // play game
+private:
+	Deck m_Deck;
+	House m_House;
+	std::vector<Player> m_Player;
+};
+
+#endif // !GAME_H
